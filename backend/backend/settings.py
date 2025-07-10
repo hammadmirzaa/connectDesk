de7a2186cd4755451218069b65874e8463576bdf
hammadmirzaa <hamadbaig25@gmail.com>
2025-07-10 22:51:56 +0500

    Fixed CORS Error

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -204,7 +204,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://connect-desk-olive.vercel.app/",
+    "https://connect-desk-olive.vercel.app",
 ]
 
 
