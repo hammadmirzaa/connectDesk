232264d8e052197edc44c22c7a2c413783ae087d
hammadmirzaa <hamadbaig25@gmail.com>
2025-07-10 18:59:59 +0500

    deploy process

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,9 +39,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
-         'OPTIONS': {
-            'charset': 'utf8mb4',  
-        },
+        #  'OPTIONS': {
+        #     'charset': 'utf8mb4',  
+        # },
     }
 }
 
